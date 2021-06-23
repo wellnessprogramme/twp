b422130147a2f63802e387a226eb8b4c3cceb818
Daniel Soares <daniel.soares@me.com>
2021-06-23 11:52:00 -0300

    feat: add pages

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,13 +5,27 @@ import logo from '@/assets/logo.svg'
 
 export default function Nav() {
   return (
-    <div className="h-16 text-white bg-primary">
-      <div className="flex items-center justify-between max-w-screen-xl px-6 py-2 mx-auto">
-        <Image src={logo} alt="" />
-        <div className="lg:hidden">
-          <AiOutlineMenu size={22} />
+    <>
+      <div className="z-50 h-16 text-white bg-primary">
+        <div className="flex items-center justify-between max-w-screen-xl px-6 py-2 mx-auto">
+          <Image src={logo} alt="" />
+          <div className="lg:hidden">
+            <AiOutlineMenu size={22} />
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* <nav className="absolute z-40 w-2/3 min-h-screen p-6 bg-white bg-opacity-95">
+        <ul>
+          <li>a</li>
+          <li>a</li>
+          <li>c</li>
+          <li>a</li>
+          <li>a</li>
+          <li>a</li>
+          <li>a</li>
+        </ul>
+      </nav> */}
+    </>
   )
 }
---
+++ src/pages/shop/index.js
@@ -0,0 +1,24 @@
+import FreeProgrammes from '@/components/FreeProgrammes'
+import { Layout } from '@/components/Layout'
+import { directus } from '@/lib/directus'
+
+export default function Shop({ freeProgrammes }) {
+  return (
+    <Layout>
+      <FreeProgrammes programmes={freeProgrammes} />
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const { data: freeProgrammes } = await directus.items('programmes').readMany({
+    fields: ['id', 'title', 'slug', 'featured_image'],
+    filter: {
+      status: { _eq: 'published' },
+      is_free: { _eq: true }
+    },
+    limit: '3'
+  })
+
+  return { props: { freeProgrammes } }
+}
